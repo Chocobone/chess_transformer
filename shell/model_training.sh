6a58928bfd057b8b645db0be5504b09eaf9f511b
chocobone <yooho1030@naver.com>
2026-05-14 11:55:02 +0900

    fix: wrong slurm partiton name and node name

--- shell/model_training.sh
+++ shell/model_training.sh
@@ -4,8 +4,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
-#SBATCH -p batch_ugrad_advisor_x_
-#SBATCH -w moana_u8
+#SBATCH -p batch_ugrad_advisor_x
+#SBATCH -w moana-u8
 #SBATCH -t 1-0
 #SBATCH -o logs/slurm-%A.out
 
@@ -14,5 +14,7 @@
 source ~/.bashrc
 conda activate training
 
+cd /data/yho7374/repos/chess_rust/training
+python transformer.py
 
 exit 0
